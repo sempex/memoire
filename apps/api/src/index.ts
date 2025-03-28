b1ca588ecdbdcacbf496ef5d6c69279a7bb2a4de
Tim Reber <metimreber@gmail.com>
2025-03-28 12:17:53 +0100

    fix: return something on /

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import cors from 'cors';
-import { createExpressMiddleware } from '@trpc/server/adapters/express';
-import { userRouter } from './routers/user';
-import { router } from './trpc/trpc';
-import { clerkMiddleware, getAuth } from '@clerk/express';
-import 'dotenv/config';
-import { s3Router } from './routers/s3';
-
+import express from "express";
+import cors from "cors";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { userRouter } from "./routers/user";
+import { router } from "./trpc/trpc";
+import { clerkMiddleware, getAuth } from "@clerk/express";
+import "dotenv/config";
+import { s3Router } from "./routers/s3";
 
 export const appRouter = router({
-  s3: s3Router
+  s3: s3Router,
 });
 
 export type AppRouter = typeof appRouter;
@@ -18,15 +17,23 @@ const app = express();
 const PORT = 4000;
 
 // Enable CORS
-app.use(cors({
-  origin: ['http://localhost:3000'], // Allow your Next.js app origin
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"], // Allow your Next.js app origin
+    credentials: true,
+  }),
+);
 
 // Add tRPC API endpoint
 app.use(clerkMiddleware());
 
-const createContext = ({ req, res }: { req: express.Request; res: express.Response }) => {
+const createContext = ({
+  req,
+  res,
+}: {
+  req: express.Request;
+  res: express.Response;
+}) => {
   const auth = getAuth(req); // Extract authentication details
   return { auth, req, res }; // Pass authentication to context
 };
@@ -34,13 +41,18 @@ const createContext = ({ req, res }: { req: express.Request; res: express.Respon
 export type Context = ReturnType<typeof createContext>;
 
 app.use(
-  '/api/trpc',
+  "/api/trpc",
   createExpressMiddleware({
     router: appRouter,
     createContext,
-  })
+  }),
 );
+
+app.use("/", (req, res) => {
+  res.send("memoire backend");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
